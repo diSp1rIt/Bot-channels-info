52f7f7b1abd410e4a04b264c5019a46d32bc4e96
lev2454 <lewartamonov@gmail.com>
2020-06-17 16:23:18 +0200

    Initial commit

--- registration.py
+++ registration.py
@@ -1,8 +1,18 @@
-import telethon
-import asyncio
+from telethon import TelegramClient, sync
+from cfg_loader import *
+from decorators import async_call
 
-loop = asyncio.new_event_loop()
-client = telethon.TelegramClient('bot', )
+client_data = load_configs()
+API_ID = client_data['API_ID']
+API_HASH = client_data['API_HASH']
+client = TelegramClient('bot', API_ID, API_HASH)
 
-async def send_code(phone):
-    pass
+
+@async_call
+def send_code(phone: str):
+    client.send_code_request(phone)
+
+
+@async_call
+def sing_in(phone: str, code: int):
+    client.sign_in(phone, code)
