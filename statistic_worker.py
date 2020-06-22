f24d015d128a77e41d9b4912965f74dc45a9146c
lev2454 <lewartamonov@gmail.com>
2020-06-22 19:40:12 +0200

    Add delete function

--- statistic_worker.py
+++ statistic_worker.py
@@ -32,7 +32,7 @@ def get_current_info(channel):
     return text
 
 
-def do_record(channel):
+async def do_record(channel):
     new_channel = Channel()
     for i in range(len(data_list)):
         if 'full_chat' in data_list[i]:
@@ -44,6 +44,19 @@ def do_record(channel):
                 exec(f'new_channel{data_list[i].split("chats[0]")[1]} = channel.{data_list[i]}')
     db_ses.add(new_channel)
     db_ses.commit()
+
+
+async def delete_data(channel_id: int, delete_from_channels=False, delete_from_posts=False):
+    if delete_from_channels:
+        result = db_ses.query(Channel).filter(Channel.id == channel_id)
+        for channel in result:
+            db_ses.delete(channel)
+        db_ses.commit()
+    if delete_from_posts:
+        result = db_ses.query(Post).filter(Post.channel_id == channel_id)
+        for channel in result:
+            db_ses.delete(channel)
+        db_ses.commit()
 
 
 async def load_channels(client):
